68772294849f04a9d7bf4ff274d0848ce9e2a03c
Mete Ersu <meteersu1@gmail.com>
2023-07-19 12:11:01 +0300

    2. commit

--- JUnit_Practice/src/test/java/tests/junit_Practice/P4_Assertions.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P4_Assertions.java
@@ -1,12 +1,54 @@
 package tests.junit_Practice;
 
-public class P4_Assertions {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
-    //1) Bir class oluşturun: BestBuyAssertions
-    //2) https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak
+import java.security.PublicKey;
+
+public class P4_Assertions extends TestBase {
+
+
+    // https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak
     //   asagidaki testleri yapin
+
+
+
+
+    @Test
     //○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
+        public void urlTest (){
+        driver.get("https://www.bestbuy.com/");
+        String expectedUrl = "https://www.bestbuy.com/";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl,actualUrl);
+
+    }
+    @Test
     //○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
+    public void titleTest (){
+        driver.get("https://www.bestbuy.com/");
+        String unexpectedIcerik = "Rest";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertFalse(actualTitle.contains(unexpectedIcerik));
+    }
+
+    @Test
     //○ logoTest => BestBuy logosunun görüntülendigini test edin
+    public void logoTest (){
+        driver.get("https://www.bestbuy.com/");
+        Assert.assertTrue(driver.findElement(By.className("logo")).isDisplayed());
+    }
+
+    @Test
     //○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
+    public void francaisLinkTest (){
+        driver.get("https://www.bestbuy.com/");
+        Assert.assertTrue(driver.findElement(By.xpath("//button[@lang='fr']")).isDisplayed());
+    }
+
+
 }
