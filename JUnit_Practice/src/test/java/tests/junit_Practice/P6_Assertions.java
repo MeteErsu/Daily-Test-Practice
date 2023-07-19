17ffb59289edd6b26721ae06a331874bbac3ba9e
Mete Ersu <meteersu1@gmail.com>
2023-07-19 13:23:56 +0300

    2. commit

--- JUnit_Practice/src/test/java/tests/junit_Practice/P6_Assertions.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P6_Assertions.java
@@ -1,10 +1,28 @@
 package tests.junit_Practice;
 
-public class P6_Assertions {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utilities.TestBase;
 
-    //1. Bir Class olusturalim YanlisEmailTesti
-    //2. http://automationpractice.com/index.php sayfasina gidelim
-    //3. Sign in butonuna basalim
-    //4. Email kutusuna @isareti olmayan bir mail yazip enter’a bastigimizda “Invalid
-    //email address” uyarisi ciktigini test edelim
+public class P6_Assertions extends TestBase {
+
+
+    @Test
+    public void yanlisEmailTesti (){
+        //1. Bir Class olusturalim YanlisEmailTesti
+        //2. https://www.n11.com/ sayfasina gidelim
+        driver.get("https://www.n11.com/");
+        //3. Giris yap butonuna basalim
+        driver.findElement(By.xpath("//a[@class='btnSignIn']")).click();
+        wait(2);
+        //4. Email kutusuna @isareti olmayan bir mail yazip enter’a bastigimizda
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("metegmail.com"+ Keys.ENTER);
+        // “Lütfen geçerli bir e-posta adresi girin.” uyarisi ciktigini test edelim
+        wait(2);
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@data-errormessagefor='email']")).isDisplayed());
+
+    }
+
 }
