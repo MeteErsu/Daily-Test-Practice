0c4ee223df6b3ecb9c493932b4d97635f74606c6
Mete Ersu <meteersu1@gmail.com>
2023-07-19 12:33:42 +0300

    2. commit

--- JUnit_Practice/src/test/java/tests/junit_Practice/P5_Assertions.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P5_Assertions.java
@@ -1,12 +1,46 @@
 package tests.junit_Practice;
 
-public class P5_Assertions {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class P5_Assertions extends TestBase {
 
     //1) Bir class oluşturun: YoutubeAssertions
     //2) https://www.youtube.com adresine gidin
     //3) Aşağıdaki adları kullanarak 4 test metodu oluşturun ve gerekli testleri yapin
-    //○ titleTest => Sayfa başlığının “YouTube” oldugunu test edin
-    //○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
-    //○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+    @Test
+    public void titleTest() {
+        //○ titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+        driver.get("https://www.youtube.com");
+        String expectedTitle = "YouTube";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+    @Test
+    public void imageTest() {
+        //○ imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+        driver.get("https://www.youtube.com");
+        Assert.assertTrue(driver.findElement(By.xpath("(//div[@class='style-scope ytd-topbar-logo-renderer'])[1]")).isDisplayed());
+    }
+    @Test
+    public void searchBoxTest () {
+        //○ Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+        driver.get("https://www.youtube.com");
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@id='search']")).isEnabled());
+
+    }
     //○ wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+
+    @Test
+    public void wrongTitleTest (){
+        driver.get("https://www.youtube.com");
+        String unexpectedTitle = "youtube";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertNotEquals(unexpectedTitle,actualTitle);
+
+    }
 }
