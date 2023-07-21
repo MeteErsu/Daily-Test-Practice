eba49db46f6f151b2fcba78f1ce04009d6e20a29
Mete Ersu <meteersu1@gmail.com>
2023-07-21 12:22:44 +0300

    21.07

--- JUnit_Practice/src/test/java/tests/junit_Practice/P13_Authentication.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P13_Authentication.java
@@ -1,14 +1,32 @@
 package tests.junit_Practice;
 
-public class P13_Authentication {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
 
-    //1- Bir class olusturun : BasicAuthentication
-    //2- https://the-internet.herokuapp.com/basic_auth
-    //sayfasina gidin
-    //3- asagidaki yontem ve test datalarini kullanarak
-    //authentication’i yapin
-    //Html komutu : https://username:password@URL
-    //Username : admin
-    //password : admin
-    //4- Basarili sekilde sayfaya girildigini dogrulayin
+public class P13_Authentication extends TestBase {
+
+    @Test
+    public void basicAuthentication (){
+
+        //1- Bir class olusturun : BasicAuthentication
+        //2- https://the-internet.herokuapp.com/basic_auth sayfasina gidin
+
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+        //3- asagidaki yontem ve test datalarini kullanarak authentication’i yapin
+
+        //Html komutu : https://username:password@URL
+        //Username : admin
+        //password : admin
+        //4- Basarili sekilde sayfaya girildigini dogrulayin
+
+        WebElement basariliGirisYazisi = driver.findElement(By.tagName("p"));
+        Assert.assertTrue(basariliGirisYazisi.isDisplayed());
+        wait(3);
+
+    }
+
 }
