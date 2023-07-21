9614de91715ef12d71835f5fbd0e3685e7498b1f
Mete Ersu <meteersu1@gmail.com>
2023-07-21 12:04:16 +0300

    21.07

--- JUnit_Practice/src/test/java/tests/junit_Practice/P11_HandleDropdown.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P11_HandleDropdown.java
@@ -1,19 +1,50 @@
 package tests.junit_Practice;
 
-public class P11_HandleDropdown {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
-    //1. http://zero.webappsecurity.com/ Adresine gidin
-    //2. Sign in butonuna basin
-    //3. Login kutusuna “username” yazin
-    //4. Password kutusuna “password.” yazin
-    //5. Sign in tusuna basin
-    //6. Pay Bills sayfasina gidin
-    //7. “Purchase Foreign Currency” tusuna basin
-    //8. “Currency” drop down menusunden Eurozone’u secin
-    //9. “amount” kutusuna bir sayi girin
-    //10. “US Dollars” in secilmedigini test edin
-    //11. “Selected currency” butonunu secin
-    //12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
-    //13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini
-    //kontrol edin.
+public class P11_HandleDropdown extends TestBase {
+
+    @Test
+    public void dropDownTest (){
+        //1. http://zero.webappsecurity.com/ Adresine gidin
+
+        driver.get("http://zero.webappsecurity.com/");
+
+        //2. Sign in butonuna basin
+
+        driver.findElement(By.xpath("//button[@id='signin_button']")).click();
+
+        //3. Login kutusuna “username” yazin
+
+        driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys("username");
+        wait(2);
+
+        //4. Password kutusuna “password.” yazin
+
+        driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("password");
+        wait(2);
+
+        //5. Sign in tusuna basin
+
+        driver.findElement(By.xpath("//input[@name='submit']")).click();
+
+        //6. Pay Bills sayfasina gidin
+
+
+
+        //7. “Purchase Foreign Currency” tusuna basin
+        //8. “Currency” drop down menusunden Eurozone’u secin
+        //9. “amount” kutusuna bir sayi girin
+        //10. “US Dollars” in secilmedigini test edin
+        //11. “Selected currency” butonunu secin
+        //12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
+        //13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini
+        //kontrol edin.
+
+
+
+    }
+
 }
--- JUnit_Practice/src/test/java/tests/junit_Practice/P12_Alert.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P12_Alert.java
@@ -1,21 +1,93 @@
 package tests.junit_Practice;
 
-public class P12_Alert {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
-    //3 test method'u olusturup asagidaki gorevi tamamlayin
-    //1. Test
-    //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
-    //- 1.alert'e tiklayin
-    //- Alert'deki yazinin "I am a JS Alert" oldugunu test edin
-    //- OK tusuna basip alert'i kapatin
-    //2.Test
-    //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
-    //- 2.alert'e tiklayalim
-    //- Cancel'a basip, cikan sonuc yazisinin "You clicked: Cancel" oldugunu test edin
-    //3.Test
-    //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
-    //- 3.alert'e tiklayalim
-    //- Cikan prompt ekranina "Abdullah" yazdiralim
-    //- OK tusuna basarak alert'i kapatalim
-    //- Cikan sonuc yazisinin Abdullah icerdigini test edelim
+public class P12_Alert extends TestBase {
+
+     @Test
+     public void test01(){
+      //1. Test
+      //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
+
+      driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+      //- 1.alert'e tiklayin
+
+      driver.findElement(By.xpath("//button[@onclick='jsAlert()']")).click();
+
+      //- Alert'deki yazinin "I am a JS Alert" oldugunu test edin
+
+      String expectedAlert = "I am a JS Alert";
+      String actualAlert = driver.switchTo().alert().getText();
+
+      Assert.assertEquals(expectedAlert,actualAlert);
+      wait(3);
+
+      //- OK tusuna basip alert'i kapatin
+
+      driver.switchTo().alert().accept();
+
+     }
+
+     @Test
+     public void test02(){
+
+      //2.Test
+      //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
+
+      driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+      //- 2.alert'e tiklayalim
+
+      driver.findElement(By.xpath("//button[@onclick='jsConfirm()']")).click();
+      wait(2);
+
+      //- Cancel'a basip, cikan sonuc yazisinin "You clicked: Cancel" oldugunu test edin
+
+      driver.switchTo().alert().dismiss();
+      wait(3);
+
+      String expectedSonucYazisi = "You clicked: Cancel";
+      String actualSonucYazisi = driver.findElement(By.xpath("//p[@id='result']")).getText();
+
+      Assert.assertEquals(expectedSonucYazisi,actualSonucYazisi);
+     }
+     @Test
+     public void test03(){
+
+      //3.Test
+      //- https://the-internet.herokuapp.com/javascript_alerts adresine gidin
+
+      driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+      //- 3.alert'e tiklayalim
+
+      driver.findElement(By.xpath("//button[@onclick='jsPrompt()']")).click();
+      wait(2);
+
+      //- Cikan prompt ekranina "Abdullah" yazdiralim
+
+      driver.switchTo().alert().sendKeys("Abdullah");
+      wait(2);
+
+      //- OK tusuna basarak alert'i kapatalim
+
+      driver.switchTo().alert().accept();
+      wait(2);
+
+      //- Cikan sonuc yazisinin Abdullah icerdigini test edelim
+
+      String expectedIcerik = "Abdullah";
+      String actualSonucIcerik = driver.findElement(By.xpath("//p[@id='result']")).getText();
+
+      Assert.assertTrue(actualSonucIcerik.contains(expectedIcerik));
+
+     }
+
+
+
+
 }
