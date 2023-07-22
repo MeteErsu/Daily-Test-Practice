f1d104d71c90fb361e55a4dddbf63fa6a415911f
Mete Ersu <meteersu1@gmail.com>
2023-07-22 12:10:22 +0300

    22.07

--- JUnit_Practice/src/test/java/tests/junit_Practice/P17_HandleWindows.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P17_HandleWindows.java
@@ -1,15 +1,71 @@
 package tests.junit_Practice;
 
-public class P17_HandleWindows {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
-    //● Tests package’inda yeni bir class olusturun: WindowHandle2
-    //● https://the-internet.herokuapp.com/windows adresine gidin.
-    //● Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
-    //● Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
-    //● Click Here butonuna basın.
-    //● Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu
-    //dogrulayin.
-    //● Sayfadaki textin “New Window” olduğunu doğrulayın.
-    //● Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet”
-    //olduğunu doğrulayın.
+import java.util.Set;
+
+public class P17_HandleWindows extends TestBase {
+
+
+    @Test
+    public void windowHandle2 (){
+
+        //● Tests package’inda yeni bir class olusturun: WindowHandle2
+        //● https://the-internet.herokuapp.com/windows adresine gidin.
+
+        driver.get("https://the-internet.herokuapp.com/windows");
+        String ilkWHD = driver.getWindowHandle();
+
+        //● Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+
+        String expectedText = "Opening a new window";
+        String actualText = driver.findElement(By.tagName("h3")).getText();
+        Assert.assertEquals(expectedText,actualText);
+
+        //● Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+
+        String expectedTitleIlk = "The Internet";
+        String actualTitleIlk = driver.getTitle();
+        Assert.assertEquals(expectedTitleIlk,actualTitleIlk);
+
+        //● Click Here butonuna basın.
+
+        driver.findElement(By.xpath("//a[@href='/windows/new']")).click();
+
+        //● Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
+
+        Set<String> wHDSeti = driver.getWindowHandles();
+        String ikinciWHD = "";
+        for (String each:wHDSeti
+             ) {
+            if (!each.equals(ilkWHD)){
+                ikinciWHD=each;
+            }
+        }
+
+        driver.switchTo().window(ikinciWHD);
+        String expectedTitleIkinci = "New Window";
+        String actualTitleIkinci = driver.getTitle();
+        Assert.assertEquals(actualTitleIkinci,actualTitleIkinci);
+
+        //● Sayfadaki textin “New Window” olduğunu doğrulayın.
+
+        String expectedTextIkinci = "New Window";
+        String actualTextIkinci = driver.findElement(By.tagName("h3")).getText();
+        Assert.assertEquals(expectedTextIkinci,actualTextIkinci);
+
+        //● Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet”
+        //olduğunu doğrulayın.
+
+        driver.switchTo().window(ilkWHD);
+
+        String expectedTitleSon = "The Internet";
+        String actualTitleSon = driver.getTitle();
+        Assert.assertEquals(expectedTitleSon,actualTitleSon);
+
+    }
+
 }
