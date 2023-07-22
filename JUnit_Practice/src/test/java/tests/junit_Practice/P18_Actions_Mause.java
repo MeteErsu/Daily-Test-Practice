0aab71e57c93dfc00849fa9d6c42a96a9bd616df
Mete Ersu <meteersu1@gmail.com>
2023-07-22 22:47:58 +0300

    22.07

--- JUnit_Practice/src/test/java/tests/junit_Practice/P18_Actions_Mause.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P18_Actions_Mause.java
@@ -1,12 +1,30 @@
 package tests.junit_Practice;
 
-public class P18_Actions_Mause {
+import org.junit.Test;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
 
-    //1- Yeni bir class olusturalim: MouseActions1
-    //2- https://the-internet.herokuapp.com/context_menu sitesine gidin
-    //3- Cizili alan uzerinde sag click yapin
-    //4- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
-    //5- Tamam diyerek alert’i kapatalim
-    //6- Elemental Selenium linkine tiklayalim
-    //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini test edelim
+import javax.swing.*;
+
+public class P18_Actions_Mause extends TestBase {
+
+    @Test
+    public void mouseActions1 (){
+
+        //1- Yeni bir class olusturalim: MouseActions1
+        //2- https://the-internet.herokuapp.com/context_menu sitesine gidin
+
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+
+        //3- Cizili alan uzerinde sag click yapin
+
+        Actions action = new Actions(driver)
+
+        //4- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
+        //5- Tamam diyerek alert’i kapatalim
+        //6- Elemental Selenium linkine tiklayalim
+        //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini test edelim
+
+    }
+
 }
