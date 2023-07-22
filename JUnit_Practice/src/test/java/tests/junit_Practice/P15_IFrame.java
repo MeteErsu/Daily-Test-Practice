fe268c92cbd00c07a1227688e705a442a2d699a7
Mete Ersu <meteersu1@gmail.com>
2023-07-22 10:39:22 +0300

    22.07

--- JUnit_Practice/src/test/java/tests/junit_Practice/P15_IFrame.java
+++ JUnit_Practice/src/test/java/tests/junit_Practice/P15_IFrame.java
@@ -1,11 +1,47 @@
 package tests.junit_Practice;
 
-public class P15_IFrame {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
 
-    //1) http://demo.guru99.com/test/guru99home/ sitesine gidiniz
-    //2) sayfadaki iframe sayısını bulunuz.
-    //3) ilk iframe'deki (Youtube) play butonuna tıklayınız.
-    //4) ilk iframe'den çıkıp ana sayfaya dönünüz
-    //5) ikinci iframe'deki (Jmeter Made Easy) linke
-    //(https://www.guru99.com/live-selenium-project.html) tıklayınız
+import java.util.List;
+
+public class P15_IFrame extends TestBase {
+
+    @Test
+    public void iframeTest (){
+
+        //1) http://demo.guru99.com/test/guru99home/ sitesine gidiniz
+
+        driver.get("http://demo.guru99.com/test/guru99home/");
+
+        //2) sayfadaki iframe sayısını bulunuz.
+
+        List<WebElement> iframeList = driver.findElements(By.tagName("iframe"));
+        int iframeCount = iframeList.size();
+        System.out.println(iframeCount);
+        wait(3);
+
+        //3) ilk iframe'deki (Youtube) play butonuna tıklayınız.
+
+        driver.switchTo().frame(0);
+
+        driver.findElement(By.xpath("//button[@aria-label='Oynat']")).click();
+        wait(3);
+
+        //4) ilk iframe'den çıkıp ana sayfaya dönünüz
+
+        driver.switchTo().defaultContent();
+
+        //5) ikinci iframe'deki (Jmeter Made Easy) linke (https://www.guru99.com/live-selenium-project.html) tıklayınız
+
+        driver.switchTo().frame(1);
+        driver.findElement(By.xpath("//a[@href='http://www.guru99.com/live-selenium-project.html']")).click();
+        wait(3);
+
+
+
+    }
+
 }
